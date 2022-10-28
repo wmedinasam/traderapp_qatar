3c6e67efddc8816d728e9692ef9d2aaf826424aa
Alejandro0choa <mochoar2@miumg.edu.gt>
2022-10-28 00:13:38 -0600

    webapiTest

---
+++ webapiTest/TestUsuariosController.cs
@@ -0,0 +1,31 @@
+﻿using webapi.Models;
+using webapi.Controllers;
+using System.Collections.Generic;
+using NuGet.Protocol;
+using Microsoft.AspNetCore.Mvc.Testing;
+using webapi;
+using System.Net.Http.Json;
+
+namespace webapiTest
+{
+    [TestClass]
+    public class TestUsuariosController
+    {
+        private HttpClient _client;
+
+        
+        [TestMethod]
+        public async Task GetUsuarioligas_ShouldReturnCorrectUsuarioLiga() 
+        {
+            // expected result.
+            string expectedString = "{\"id\":1,\"nombre\":\"Admin\",\"pass\":\"123\",\"email\":\"admin@admin.com\",\"status\":1,\"tipo\":1,\"tipoNavigation\":null,\"fasegruposResultados\":[],\"usuarioligas\":[]}";
+
+            // test
+            var webAppFactory = new WebApplicationFactory<Program>();
+            _client = webAppFactory.CreateDefaultClient();
+            var response = await _client.GetAsync("/api/Usuarios/1");
+            var stringResult = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual(expectedString, stringResult);
+        }
+    }
+}
